d899e3f30896101db06e0db3eef06bb68639ddac
fah-6k8 <64428946+fah-6k8@users.noreply.github.com>
2022-03-23 13:38:08 -0300

    Add files via upload

---
+++ AplicacaoTesteFront/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AplicacaoTesteFront.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AplicacaoTesteFront/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace AplicacaoTesteFront.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
